bcc1f029ba705c479f70275e4a05a2a4657a3a26
= <valane91@gmail.com>
2018-08-30 17:30:26 +0200

    2 commit

--- models/Model.php
+++ models/Model.php
@@ -1,5 +1,6 @@
 <?php
-$config = include('/config/dev.php');
+
+
 class Model 
 {
     const DB_NAME = 'my_cms';
@@ -7,15 +8,17 @@ class Model
     const PASS = '1234';
     protected $dbConnect;
     protected $tableName;
+    
 
     function __construct() {
+        $ini = parse_ini_file('/var/www/html/config/dev.ini');
         //
         /*try {
             
         } catch (Exception $e) {
             die ('ERReyurrerz' . $e->getMessage());
         }*/
-        $this->dbConnect = new PDO('mysql:host=localhost; dbname=' . self::DB_NAME ,self::USER, self::PASS);
+        $this->dbConnect = new PDO('mysql:host=' . $ini["db_host"] . '; dbname=' . $ini["db_name"] ,$ini["db_user"], $ini["db_password"]);
     }
 
     public function getAll() {
